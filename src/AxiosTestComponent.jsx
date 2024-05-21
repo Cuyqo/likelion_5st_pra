6ded2df5d8a8963a42fb64c2142138d94a0f3270
jeongin <saon0410@gmail.com>
2024-05-21 23:05:07 +0900

    2

--- src/AxiosTestComponent.jsx
+++ src/AxiosTestComponent.jsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
 function AxiosTestComponent() {
-  //이 컴포넌트가 맨 처음 로딩돼서 렌더링 되자 마자
-  // axios로 샘플 json 데이터 받아오기
   axios
     .post('https://reqres.in/api/login', {
       email: 'eve.holt@reqres.in',
@@ -13,9 +11,7 @@ function AxiosTestComponent() {
     .catch(function (error) {
       console.log(error);
     });
-  //자바스크립트 구문
 
-  //return (JSX)구문
   return <div>테스트 1</div>;
 }
 export default AxiosTestComponent;
